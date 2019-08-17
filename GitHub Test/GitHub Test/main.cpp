b1f201e3a5804148819e40a9c3bee5bcf402b4bc
Kevin Tran <tran.kevin32@gmail.com>
2019-08-16 22:53:39 -0500

    First time pushing to GitHub

---
+++ GitHub Test/GitHub Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHub Test
+//
+//  Created by Kevin Tran on 8/16/19.
+//  Copyright © 2019 Kevin Tran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
